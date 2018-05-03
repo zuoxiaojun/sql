4427abd2a11044cf51da6d446c45903e7f794637
zuoxiaojun <zuoxiaojun111@163.com>
2018-05-03 11:43:56 +0800

    20180503
    20180503

--- EPC付款单付款账户数据修复.sql
+++ EPC付款单付款账户数据修复.sql
@@ -1,18 +1,19 @@
 
 付款账户->getColValue(bd_bankaccsub,name,PK_BANKACCSUB,payaccount);
 
-select pk_paybill,modifier,modifiedtime from ap_paybill where billno='80501FK18040003' for update
-
 /*EPC付款单付款账户数据修复*/
 
 select payaccount
   from ap_payitem
  where pk_paybill =
-       (select pk_paybill from ap_paybill where billno = '80501FK18040043')
+       (select pk_paybill from ap_paybill where billno = '80501FK18050001')
    for update 
  --1001D1100000009DZLZ4    加拿大元 
 --  1001B110000000016097  人民币
 
+/**修复结算单数据*/
+ 
+select accountnum,pk_account from cmp_detail  where pk_detail='1001D110000000A5QVYW'   for update 
 
-select accnum,pk_bankaccsub from  bd_bankaccsub  where  pk_bankaccsub ='1001D1100000009DZLZ4'
+
 
--- EPC付款单付款账户数据修复.~sql
+++ EPC付款单付款账户数据修复.~sql
@@ -7,6 +7,12 @@ select pk_paybill,modifier,modifiedtime from ap_paybill where billno='80501FK180
 
 select payaccount
   from ap_payitem
- where pk_paybill = (select pk_paybill
-                       from ap_paybill
-                      where billno = '80501FK18040006') for update   --1001B110000000016097    
+ where pk_paybill =
+       (select pk_paybill from ap_paybill where billno = '80501FK18040043')
+   for update 
+ --1001D1100000009DZLZ4    加拿大元 
+--  1001B110000000016097  人民币
+
+
+select accnum,pk_bankaccsub from  bd_bankaccsub  where  pk_bankaccsub ='1001D1100000009DZLZ4'
+
