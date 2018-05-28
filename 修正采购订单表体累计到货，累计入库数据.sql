7d528afcdb4e60e2173d108211d2effdef31966c
左小军 <zuoxiaojun111@163.com>
2018-05-28 16:46:27 +0800

    1
    1

---
+++ 修正采购订单表体累计到货，累计入库数据.sql
@@ -0,0 +1,5 @@
+/*修正采购订单表体累计到货，累计入库数据*/
+select nbackarrvnum,nbackstorenum,naccumarrvnum,naccumstorenum
+  from po_order_b
+ where pk_order =
+       (select pk_order from po_order where vbillcode = '81600CD20170904008'and dr=0 and bislatest='Y') --for update 
