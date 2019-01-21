0ef9a544d2103ccde02d5a20c5730cdc0d041708
zuoxiaojun <zuoxiaojun111@163.com>
2019-01-21 13:39:59 +0800

    20190121

--- 单据生成的临时数据查询.sql
+++ 单据生成的临时数据查询.sql
@@ -3,6 +3,6 @@
 select *
 --src_relationid, operateflag, des_relationid, dr
   from fip_operatinglog
- where src_org = (select pk_org from org_orgs where code = '834') 
+ where src_org in (select pk_org from org_orgs where code = '833') 
  
- delete from fip_operatinglog where src_org = (select pk_org from org_orgs where code = '834') 
+ delete from fip_operatinglog where src_org in (select pk_org from org_orgs where code = '833') 
