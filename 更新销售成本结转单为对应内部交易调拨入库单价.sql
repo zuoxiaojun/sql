2856171eb28634fb1b953a4a7159736e469826e4
zuoxiaojun <zuoxiaojun111@163.com>
2019-02-15 12:00:03 +0800

    0215

--- 更新销售成本结转单为对应内部交易调拨入库单价.sql
+++ 更新销售成本结转单为对应内部交易调拨入库单价.sql
@@ -34,13 +34,26 @@ and v.nprice is not null;
 select * from v_gcl_i5itemupdate    --查询出需要修改的单据
 
 --更新ia_detailledger
-update ia_detailledger a set (a.nprice,a.nmny) = (select v.nprice,nvl(v.nprice,0)*nvl(a.nnum,0) nmny from v_gcl_i5itemupdate v where a.cbill_bid = v.cbill_bid)
-where a.dr = 0 and exists (select 1 from v_gcl_i5itemupdate v where a.cbill_bid = v.cbill_bid);
+update ia_detailledger a
+   set (a.nprice, a.nmny) =
+       (select v.nprice, nvl(v.nprice, 0) * nvl(a.nnum, 0) nmny
+          from v_gcl_i5itemupdate v
+         where a.cbill_bid = v.cbill_bid)
+ where a.dr = 0
+   and exists
+ (select 1 from v_gcl_i5itemupdate v where a.cbill_bid = v.cbill_bid);
 
 --更新存货核算I5 销售成本结转
-update ia_i5bill_b b set (b.nprice,b.nmny) = (select v.nprice,nvl(v.nprice,0)*nvl(b.nnum,0) nmny from v_gcl_icoutitem v where b.cicitemid = v.coutitemid)
-where b.dr = 0 and b.pk_org = '1001D1100000000QYRNK' and b.nprice is null 
-and exists (select 1 from v_gcl_icoutitem v where b.cicitemid = v.coutitemid);
+update ia_i5bill_b b
+   set (b.nprice, b.nmny) =
+       (select v.nprice, nvl(v.nprice, 0) * nvl(b.nnum, 0) nmny
+          from v_gcl_icoutitem v
+         where b.cicitemid = v.coutitemid)
+ where b.dr = 0
+   and b.pk_org = '1001D1100000000QYRNK'
+   and b.nprice is null
+   and exists
+ (select 1 from v_gcl_icoutitem v where b.cicitemid = v.coutitemid);
 
 
 
