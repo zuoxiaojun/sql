c516e2591a27872fba3129ce9368f51dd43a7f13
zuoxiaojun <zuoxiaojun111@163.com>
2019-02-20 10:06:58 +0800

    0220

--- 结算价格维护.sql
+++ 结算价格维护.sql
@@ -1,25 +1,25 @@
 --内部结算清单，结算价格维护错误
 --修正上下游结算财务组织和成本域
-select cbillid from to_settlelist where vbillcode = '5F20181228122' --for update; --1001D110000000EBH6O9
-select crowno,
-       cupcostregionid,--上游成本域
-       cupfinanceorgid,--上游财务组织最新版本
-       cupfinanceorgvid,--上游财务组织
-       cdowncostregionid,--下游成本域
-       cdownfinanceorgid,--下游财务组织最新版本
-       cdownfinanceorgvid,--下游财务组织 
-       norigtaxprice, --结算币种含税单价 
-       nexchangerate,                        --汇率
-       ninexchangerate ,--调入方折本汇率 
-       ccurrencyid,--结算币种
-       nprice,--主本币无税单价
-       ninprice  --调入方主本币无税单价                           --1002Z0100000000001K8 美元    --1002Z0100000000001K1  人民币
-  from to_settlelist_bb                                                   --1002Z0100000000001K3  欧元
- where cbillid = '1001D110000000EBH6O9'
- --order by CCURRENCYID  for update
- and cupfinanceorgvid =(select  pk_vid  from org_financeorg_v where code='815')  --上游财务组织
-  for update 
+select cbillid from to_settlelist where vbillcode = '5F20190219040' --for update; --1001D110000000FR5PJC
 
+select crowno,
+       cupcostregionid, --上游成本域
+       cupfinanceorgid, --上游财务组织最新版本
+       cupfinanceorgvid, --上游财务组织
+       cdowncostregionid, --下游成本域
+       cdownfinanceorgid, --下游财务组织最新版本
+       cdownfinanceorgvid, --下游财务组织 
+       norigtaxprice, --结算币种含税单价 
+       nexchangerate, --汇率
+       ninexchangerate, --调入方折本汇率 
+       ccurrencyid, --结算币种
+       nprice, --主本币无税单价
+       ninprice --调入方主本币无税单价                           --1002Z0100000000001K8 美元    --1002Z0100000000001K1  人民币
+  from to_settlelist_bb --1002Z0100000000001K3  欧元
+ where cbillid = '1001D110000000FR5PJC'
+ --order by CCURRENCYID  for update
+ and cupfinanceorgvid =(select  pk_vid  from org_financeorg_v where code='813')  --上游财务组织
+  for update 
 
 
 
