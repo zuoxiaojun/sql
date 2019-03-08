078a460a3410581b7846dddbb62c9c6b0e869721
zuoxiaojun <zuoxiaojun111@163.com>
2019-03-08 14:11:35 +0800

    0308

--- 修改付款申请单状态.sql
+++ 修改付款申请单状态.sql
@@ -1,12 +1,12 @@
 select busistatus, vbillstatus, APPLYDATE,modifiedtime,modifier, pk_apply 
   from cmp_apply
- where vbillno = 'SS19012911'  and dr =0 for update
+ where vbillno = 'SS17050335'  and dr =0 for update
 
 select busistatus,
        vbillstatus,
        olcrate  
   from cmp_apply_b
- where pk_apply ='1001D110000000F2I8N6'
+ where pk_apply ='1001D11000000029KKCR'
    for update
 
 
---
+++ 修改结算单修改人.sql
@@ -0,0 +1,5 @@
+select modifier, modifiedtime
+  from cmp_settlement
+ where billcode = '84000JK19020002'
+ 
+for update 
