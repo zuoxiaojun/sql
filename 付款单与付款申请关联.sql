eadd57080a4575df03e15c3df124f37763508647
zuoxiaojun <zuoxiaojun111@163.com>
2019-05-10 14:10:02 +0800

    20190510

--- 付款单与付款申请关联.sql
+++ 付款单与付款申请关联.sql
@@ -5,29 +5,21 @@
  select CONTRACTNO,
         TOP_BILLID,
         TOP_BILLTYPE,
-        TOP_ITEMID,
-        TOP_TERMCH,
-        local_money_de
+        TOP_ITEMID
    from ap_payitem
-  where billno = '80100FK19040123'
+  where billno = '83300FK19040014'
     and dr = 0 
     
-1 GCL/XMBU-QT-2017-0216号  1001D110000000FGI9MH  36D1  1001D110000000FGI9MI  1001B110000000016MIS  2387.84000000
-2 -                        1001D110000000E3KIUJ  36D1  1001D110000000E3KIUK  1001D110000000E3KISU  1677.50000000
-
-
-
-
+--1	vodafone 2018 December	1001D110000000H217Y2	36D1	1001D110000000H217Y3	~	7.53000000
 
 select CONTRACTNO,
        TOP_BILLID,
        TOP_BILLTYPE,
        TOP_ITEMID,
-       TOP_TERMCH,
-       local_money_de, --构成关联的字段
+       --构成关联的字段
        dr
   from ap_payitem
- where billno = '80100FK19040159'
+ where billno = '83300FK19040013'
    and dr = 0
    for update
      ;
